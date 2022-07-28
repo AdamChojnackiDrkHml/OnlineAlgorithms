68c7f6c1bddfa56d46750add171ab4b2c6cbd06a
Adam Chojnacki <adam.chojnacki.syl@gmail.com>
2022-07-28 10:52:42 +0200

    Add universal solver creator

--- OnlineAlgorithms/internal/solver/genericSolver.go
+++ OnlineAlgorithms/internal/solver/genericSolver.go
@@ -1,14 +1,50 @@
 package solver
 
-import "fmt"
+import (
+	pagingsolver "OnlineAlgorithms/internal/solver/pagingSolver"
+	updatelistsolver "OnlineAlgorithms/internal/solver/updateListSolver"
+)
+
+type SolverTypeEnum int
+
+const (
+	Paging SolverTypeEnum = iota
+	UpdateList
+)
 
 type GenericSolver interface {
 	Serve(request int)
 	Raport() string
 }
 
-func DebugPrint(s string, p bool) {
-	if p {
-		fmt.Print(s)
+func CreateSolver(conf [4]int) []GenericSolver {
+	var gS []GenericSolver
+	debug := conf[3] == 1
+	control := conf[2] - 1
+	all := false
+	if conf[2] == 0 {
+		control = 0
+		all = true
+	}
+	for {
+		gS = append(gS, initSolver(conf[1], control, debug, conf[0]))
+		if !all || control == 2 {
+			break
+		}
+		control++
+	}
+	return gS
+}
+
+func initSolver(size, alg int, debug bool, solver int) GenericSolver {
+	switch SolverTypeEnum(solver) {
+	case Paging:
+		return pagingsolver.PagingSolver_Create(size, alg, debug)
+
+	case UpdateList:
+		return updatelistsolver.UpdateListSolver_Create(size, alg, debug)
+	default:
+		return nil
+
 	}
 }
