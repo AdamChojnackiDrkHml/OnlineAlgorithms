9c53c7859cfb2a91ef8464194f1d374525153442
AdamChojnackiDrkHml <72651971+AdamChojnackiDrkHml@users.noreply.github.com>
2022-11-14 23:10:09 +0100

    Remove old testcase

--- OnlineAlgorithms/internal/solver/genericSolver_test.go
+++ OnlineAlgorithms/internal/solver/genericSolver_test.go
@@ -1,24 +1,21 @@
 package solver
 
 import (
-	"OnlineAlgorithms/internal/utils"
 	"testing"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestCreate(t *testing.T) {
 
-	solverConfs := utils.SolverConfigS{ProblemType: 0, Size: 10, Alg: 1, Debug: true, DoAll: false}
+	// solverConfs := utils.SolverConfigS{ProblemType: 0, Size: 10, AlgP: 1, Debug: true, DoAll: false}
 
-	_, err := CreateSolver(solverConfs)
+	// _, err := CreateSolver(solverConfs)
 
-	assert.Equal(t, nil, err)
+	// assert.Equal(t, nil, err)
 
-	solverConfs = utils.SolverConfigS{ProblemType: 0, Size: 10, Alg: 7, Debug: true, DoAll: false}
+	// solverConfs = utils.SolverConfigS{ProblemType: 0, Size: 10, AlgP: 7, Debug: true, DoAll: false}
 
-	_, err = CreateSolver(solverConfs)
+	// _, err = CreateSolver(solverConfs)
 
-	assert.NotEqual(t, nil, err)
+	// assert.NotEqual(t, nil, err)
 
 }
